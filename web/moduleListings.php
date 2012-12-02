40fdf8ec14c5708a03c5073fd30f2700d9d91120
Alex Liew <alex.ljz@gmail.com>
2012-12-02 06:11:35 -0800

    updated html in modulelistings.php

--- web/moduleListings.php
+++ web/moduleListings.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn']) {
 <!DOCTYPE html>
 <html>
 <head>
-  <title>rebox</title>
+  <title>reboxed</title>
   <!-- Stylesheets -->
   <link rel="stylesheet" type="text/css" href="external/bootstrap/css/bootstrap.min.css"/>
   <link rel="stylesheet" type="text/css" href="css/master.css"/>
@@ -18,12 +18,12 @@ if (!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn']) {
 <body class="moduleListings">
   <div class="navbar navbar-fixed-top">
     <div class="navbar-inner">
-      <a class="brand" href="#">&nbsp Reboxed</a>
+      <a class="brand" href="#"><img src="images/logosmall.png" style="height: 15px; padding-left: 35px;"/></a>
       <ul class="nav pull-right">
         <li class="active"><a href="#">Dashboard</a></li>
         <li><a href="#">Profile</a></li>
         <li><a href="#">Help</a></li>
-        <li class=""><a href="logout.php">Logout</a></li>
+        <li class=""><a href="#">Logout</a></li>
       </ul>
     </div>
   </div>
@@ -31,9 +31,9 @@ if (!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn']) {
     <div class="mainScreen row-fluid">
       <div class="solutionsCol span9">
         <div class="titleText">
-          <h1 style="color: #CCC">&nbsp;Dashboard</h1>
+          <h2 style="color: #333; font-weight: 100;">Dashboard</h2>
         </div>
-        <div id="solutions_table" class="solutionsTable">
+        <div class="solutionsTable">
           <!-- Header -->
           <div class="solutionsHeader container-fluid">
             <div class="headerDetails row-fluid">
@@ -117,18 +117,18 @@ if (!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn']) {
             <div class="expand-details">
             </div>
           </div>
-          <div id="solutions_footer" class="solutionsFooter container-fluid">
+          <div class="solutionsFooter container-fluid">
             <div class="footerDetails row-fluid">
-              <a href="#">Add Solution</a>
+              <a class="white" href="#">[+] Add Solution</a>
             </div>
           </div>
 
 
         </div>
       </div>
-      <div id="devicesCol" class="devicesCol span3">
+      <div class="devicesCol span3">
         <div>
-          <h4 style="color: #CCC; padding: 10px;">Device List</h4>
+          <h4 style="color: #333; font-weight:100; padding: 10px;">Device List</h4>
         </div>
         <div class="deviceDetails green">
           <b>HTC Android 2.5</b></br>
@@ -152,11 +152,21 @@ if (!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn']) {
         </div>
       </div>
     </div>
+    <div id="poppup_div" class="popup_msg">
+      <div class="divData">
+        <a href="customizableWidget.html">Modify</a></br>
+        <a href="#">Delete</a>
+
+        <div class="blackTriangle"></div>
+        <div class="triangle"></div>
+      </div>
+    </div>
   </div>
+
+
 
   <!-- Javascript -->
   <script type="text/javascript" src="external/jQuery/jquery-1.8.3.min.js"></script>
-  <script type='text/javascript' src='https://static.firebase.com/v0/firebase.js'></script>
   <script type="text/javascript" src="js/moduleListings.js"></script>
 </body>
 </html>
