891f12f797d768e1964b3649e2d9f3b431d86bfd
Xythum <xythum@Xythums-MacBook-Air.local>
2012-12-02 01:59:58 -0800

    customizable widget page update

--- web/js/moduleListings.js
+++ web/js/moduleListings.js
@@ -3,10 +3,25 @@ $(document).ready(function() {
 	// Write all page specific javascripts here.
 	console.log("hello");
 
+	var openPopup = false;
 
-	$('.solutionsRow').click(function(){
-		console.log("test");
-		window.location = "customizableWidget.html";
+	$('html').click(function(pageEvent){
+		$('#poppup_div').css({left:leftVal,top:topVal}).hide();
+		$('.solutionsRow').removeClass('greyOut');
+	});
+
+
+	$('.solutionsRow').click(function(pageEvent){
+		$('.solutionsRow').removeClass('greyOut');
+		$(this).addClass('greyOut')
+		var height = $('#poppup_div').height();
+		var width = $('#poppup_div').width();
+		//calculating offset for displaying popup message
+		leftVal=pageEvent.pageX+20+"px"; //-(width/2)
+		topVal=pageEvent.pageY-20+"px"; //-(height/2)
+		//show the popup message and hide with fading effect
+		$('#poppup_div').css({left:leftVal,top:topVal}).show();
+		pageEvent.stopPropagation();
 	});
 
 	
