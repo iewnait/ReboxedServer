b0167fb0cd79e265b85ed8ab0840999d60acdabd
Xythum <xythum@Xythums-MacBook-Air.local>
2012-12-01 23:46:42 -0800

    updated HTML and CSS with 3rd page

--- web/js/moduleListings.js
+++ web/js/moduleListings.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
 	// Write all page specific javascripts here.
 	console.log("hello");
 
-	
-	
+
+	$('.solutionsRow').click(function(){
+		console.log("test");
+		window.location = "customizableWidget.html";
+	});
+
 	
 });
